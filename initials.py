6fbc9f28b4c2cc23492666d658954c3e0c41a5a6
Allotey Samuel Nii Adotei <niiodon6@gmail.com>
2026-05-31 10:00:56 +0000

    Complete assignment 1

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Print out my initials in stylized large letters.
+# Assignment Number: 1
+#
+# Name: Allotey Nii Samuel Adotei
+# STUDENT ID: 2425401228
+# Email: niiodon6@gmail.com
+# Grader: Carolyn
+#
+# On my honor, Allotey Nii Samuel Adotei, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    # Print the small initials and large stylized initials for NSA.
+    print()
+    print("...NSA")
+    print()
+    print("...NN........NN......SSSSSSSSSS......AAAAAAAAAAAA...")
+    print("...NNN.......NN....SS..........SS....AA........AA...")
+    print("...NNNN......NN....SS................AA........AA...")
+    print("...NN.NN.....NN....SS................AA........AA...")
+    print("...NN..NN....NN......SSSSSSSSSS......AAAAAAAAAAAA...")
+    print("...NN...NN...NN................SS....AA........AA...")
+    print("...NN....NN..NN................SS....AA........AA...")
+    print("...NN.....NN.NN................SS....AA........AA...")
+    print("...NN......NNNN...**SS..........SS**.AA........AA...")
+    print("...NN........NN...**..SSSSSSSSSS..**.AA........AA...")
+    print()
 
 
 main()
